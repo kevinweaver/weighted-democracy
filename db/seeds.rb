66d8bba2c0a5c28b12ca77d4581b16cfe1756173
Lee Quarella <lee@lucidfrog.com>
2013-01-23 15:50:34 -0500

    started seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "Creating Users..."
+users = [
+  ["Jorge", "Almeyda", "nash2609@gmail.com", "fishringcandle"],
+  ["Steve", "Crespo", "stephencrespo9@gmail.com", "charliebrowntree"],
+  ["Lee", "Quarella", "leequarella@gmail.com", "im1coolguy"]
+]
+users.each do |user|
+  User.create({
+    first_name: user[0],
+    last_name: user[1],
+    email: user[2],
+    password: user[3],
+    password_confirmation: user[3]
+    })
+end
+
+
+puts "Creating Issues..."
+Issue.create({
+  name: "Things",
+  description: "LOTS OF TEXT"
+})
