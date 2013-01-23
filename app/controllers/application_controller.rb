4efa0b911aae5cee2bc7ba24136ca3079dbc36bf
Lee Quarella <lee@lucidfrog.com>
2013-01-23 16:23:39 -0500

    Started work on layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,10 @@ class ApplicationController < ActionController::Base
     redirect_to login_path unless session[:user_id]
   end
 
+  helper_method :current_user
+  private
+  def current_user
+    @current_user = User.find_by_id(session[:user_id])
+  end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   skip_before_filter :authenticate
+  layout 'sessions'
 
   def create
     @user = User.find_by_email(params[:email])
